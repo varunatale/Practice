14e980f2aaa1bc2ebe96853ce66a928bfd0dc960
varunatale <101618289+varunatale@users.noreply.github.com>
2022-05-01 00:03:42 +0530

    Add files via upload

---
+++ Q8.java
@@ -0,0 +1,27 @@
+package String;
+
+import java.util.Scanner;
+
+public class Q8 {
+public static void main(String[] args) {
+		
+		StringBuffer  sb;
+		sb= new StringBuffer("welcome");
+		System.out.println("Enter your String");
+		StringBuffer t= new StringBuffer("welcome");
+		System.out.println(t.insert(5, "*"));
+		  t.setCharAt(0, 'H');
+	}
+
+	
+	
+	}
+
+	
+	
+	
+	
+	
+	
+
+
